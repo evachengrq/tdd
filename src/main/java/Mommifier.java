ac15113bdc47761e479d40e5b3d4bbc5330d6fe2
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-14 21:20:04 +0800

    [evacheng]feat: implemented input has less than 30% of vowels case

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -4,6 +4,24 @@ public class Mommifier {
             throw new IllegalArgumentException();
         }
 
+        int inputLength = input.length();
+        int numberOfVowels = getNumbersOfVowels(input, inputLength);
+        if (numberOfVowels < inputLength * 0.3) {
+            return input;
+        }
+
         return null;
+    }
+
+    private static int getNumbersOfVowels(String input, int inputLength) {
+        int numberOfVowels = 0;
+        for (int i = 0; i < inputLength; i++) {
+            if (input.charAt(i) == 'a' || input.charAt(i) == 'e'
+                    || input.charAt(i) == 'i' || input.charAt(i) == 'o'
+                    || input.charAt(i) == 'u') {
+                numberOfVowels += 1;
+            }
+        }
+        return numberOfVowels;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -14,8 +14,20 @@ public class MommifierTest {
     @Test
     void should_throw_when_input_is_empty() {
         Assertions.assertThrows(Exception.class, () -> {
+            Mommifier mommifier = new Mommifier();
             Mommifier.convert("");
         });
+    }
+
+    @Test
+    void should_return_itselt_if_input_has_less_than_30_percent_of_vowels() {
+        //Given
+        Mommifier mommifier = new Mommifier();
+        String input = "hyphen";
+        //When
+        String result = Mommifier.convert(input);
+        //Then
+        Assertions.assertEquals(result, "hyphen");
     }
 
 }
